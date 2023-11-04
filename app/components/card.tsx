1562fde9c42aa0b495cb2098dc008ac8a480f070
Magistra <magistraagis@gmail.com>
2023-11-04 16:23:39 +0800

    add image props

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import Image from 'next/image';
 interface Card {
     name: string;
     desc: string;
+    image: string
 }
-export default function card({name, desc}: Card) {
+export default function card({name, desc, image}: Card) {
   return (
-    <div className='w-[300px] h-[400px] border rounded-md'>
-        <div className=' bg-blue-600 h-3/6 '>
+    <div className='w-[300px] min-h-[400px] border rounded-md'>
+        <div className=' h-[200px] '>
+            <Image src={image} alt='image' width={300} height={200}/>
         </div>
         
         <div className='p-4'>
-            <h3 className='font-bold text-xl'>{name}</h3>
+            <h3 className='font-bold text-xl truncate'>{name}</h3>
             <p className='text-sm text-slate-500'>{desc}</p>
         </div>
     </div>
