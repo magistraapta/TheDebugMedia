cf4a767a6c32993a28ceb92420240cfd9d80cd39
Magistra <magistraagis@gmail.com>
2023-11-04 16:23:47 +0800

    add hover

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import Link from 'next/link'
+
 
 export default function navbar() {
   return (
     <div className='flex justify-center shadow-md'>
         <div className='flex items-center justify-between p-6 w-11/12'>
-            <p className='w-5/12 text-2xl font-bold'>
-                TheDebugMedia
-                </p>
-            <ul className='w-4/12 flex gap-9 text-md font-semibold'>
-                <li>Article</li>
-                <li>Course</li>
-                <li>About</li>
+            <Link href={'/'} className='w-5/12 '>
+            <p className='text-2xl font-bold'>TheDebugMedia</p>
+            </Link>
+            
+            <ul className='w-4/12 flex gap-9 text-md font-semibold '>
+                <li className='px-2 rounded-sm hover:bg-slate-200'>Article</li>
+                <li className='px-2 rounded-sm hover:bg-slate-200'>Course</li>
+                <li className='px-2 rounded-sm hover:bg-slate-200'>About</li>
             </ul>
             <div className='w-3/12 flex justify-end'>
                 <button>Login</button>
