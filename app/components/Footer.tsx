c23425b85b5dc09eafa99fe5f7624f2e432a1c04
Magistra <magistraagis@gmail.com>
2023-11-04 16:23:27 +0800

    add footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='flex justify-center  bg-black'>
+        <div className='p-6 w-11/12'>
+        <p className='text-white'>@magistraapta | 2023</p>
+    </div>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Content from './content'
 import Navbar from './components/navbar'
+import Footer from './components/Footer'
 export default function Home() {
   return (
     <main className="">
       <Navbar/>
       <Content/>
+      <Footer/>
     </main>
   )
 }
