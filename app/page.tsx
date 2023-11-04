ff3a97785a4a5cac019634aac10f814510656bb0
Magistra <magistraagis@gmail.com>
2023-11-04 14:00:58 +0800

    add content

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Content from './content'
 import Navbar from './components/navbar'
 export default function Home() {
   return (
     <main className="">
       <Navbar/>
+      <Content/>
     </main>
   )
 }
