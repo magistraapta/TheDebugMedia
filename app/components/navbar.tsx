2ba8af14f7fa43e56501b5f31f14ec5a29b9f118
Magistra <magistraagis@gmail.com>
2023-11-06 12:32:10 +0800

    remove list

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -9,15 +9,6 @@ export default function navbar() {
             <Link href={'/'} className='w-5/12 '>
             <p className='text-2xl font-bold'>TheDebugMedia</p>
             </Link>
-            
-            <ul className='w-4/12 flex gap-9 text-md font-semibold '>
-                <li className='px-2 rounded-sm hover:bg-slate-200'>Article</li>
-                <li className='px-2 rounded-sm hover:bg-slate-200'>Course</li>
-                <li className='px-2 rounded-sm hover:bg-slate-200'>About</li>
-            </ul>
-            <div className='w-3/12 flex justify-end'>
-                <button>Login</button>
-            </div>
         </div>
     </div>
   )
